a15409207f72b6b21d46e51d003eddd781a1fbb7
sdariogeo <sdariocolmenarez@gmail.com>
2023-08-13 20:53:26 -0300

    se modifica el codigo con el stopPropagation

--- eventos.js
+++ eventos.js
@@ -2,12 +2,19 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     
-    const div = document.getElementById("contenedor")
+    const div = document.getElementById("contenedor");
+    const boton = document.getElementById("boton");
 
     div.addEventListener("click", () => {
 
         alert("Hola! Soy el div");
 
+    });
+
+    boton.addEventListener("click", (event) => {
+
+        event.stopPropagation();
+        
     });
 
 });
