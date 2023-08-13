76b410121b5760910678b87d5da2d3da5e73a630
sdariogeo <sdariocolmenarez@gmail.com>
2023-08-13 19:35:21 -0300

    se crea la funcion para el div

--- eventos.js
+++ eventos.js
@@ -1,1 +1,14 @@
-const boton= document.getElementById("boton")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const div = document.getElementById("contenedor")
+
+    div.addEventListener("click", () => {
+
+        alert("Hola! Soy el div");
+
+    });
+
+});
+
